4dbea89c8deb5e1463f402d9d81b38132b7a76bf
Derian123 <derian.escobar@yahoo.com>
2018-07-23 18:51:22 -0400

    Binary Search tree serialize and deserialize

--- Problem3.py
+++ Problem3.py
@@ -1,5 +1,5 @@
 # Given the root to a binary tree, implement serialize(root), which serializes the tree into a string,
-# and deserialize(s), which deserializes the string back into the tree.
+# and deserialize(s), which deserialize the string back into the tree.
 
 # Node Class
 
