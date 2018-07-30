74800b04262465ad3c9e32513c2e2406c2b14d47
Derian123 <derian.escobar@yahoo.com>
2018-07-29 21:22:47 -0400

    Count unival Subtrees

---
+++ Problem7.py
@@ -0,0 +1,26 @@
+# Given a list of integers,
+# write a function that returns the largest sum of non-adjacent numbers.
+# Numbers can be 0 or negative.
+
+
+def largest_sum(numlist):
+    current_sum = 0
+    previous_sum = 0
+    for num in numlist:
+        if previous_sum > current_sum:
+            temp_sum = previous_sum
+        else:
+            temp_sum = current_sum
+        current_sum = previous_sum + num
+        previous_sum = temp_sum
+    if previous_sum > current_sum:
+        return previous_sum
+    return current_sum
+
+
+def main():
+    numlist = [2, 4, 6, 2, 5]
+    print(largest_sum(numlist))
+
+
+main()
